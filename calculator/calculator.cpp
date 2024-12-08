a1757df86acd0ea8e1cc8ba1aa1aea5d3668254a
German-GT <proobey78@gmail.com>
2024-12-08 23:28:44 +0300

    Был заменен вектор на обычную переменную для хранения числа, переменная x была объявлена нулем, отображение ошибки перед выходом(может немного недопонял задачу

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -12,14 +12,10 @@ bool ReadNumber(Number& x) {
 }
 
 void RunCalculatorCycle() {
-    std::vector<Number> memory; // Вектор для хранения значений
-    Number x;
+    Number save = 0; // Переменная для хранения значений
+    Number x = 0;
 
     using namespace std::literals;
-
-    if (!ReadNumber(x)) {
-        return; // Выход при ошибке
-    }
 
     std::string token;
     while (std::cin >> token) {
@@ -46,13 +42,13 @@ void RunCalculatorCycle() {
             x /= right;
         }
         else if (token == "s"s) {  // Сохранить в память
-            memory.push_back(x);
+            save = x;
         }
         else if (token == "l"s) {  // Загрузить из памяти
-            if (memory.empty()) {
+            if (save == 0()) {
                 std::cerr << "Error: Memory is empty" << std::endl;
             } else {
-                x = memory.back();  // Загружаем последнее сохраненное значение
+                x = save;  // Загружаем последнее сохраненное значение
             }
         }
         else if (token == "**"s) {
@@ -73,6 +69,8 @@ void RunCalculatorCycle() {
         }
         else if (token == "q"s) {
             break;
+        }
+        else if (!ReadNumber(x)) {
         }
         else {
             std::cerr << "Error: Unknown token "s << token << std::endl;
